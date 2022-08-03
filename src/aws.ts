7ec1f9eb50c7b5252fc8fb1d66e0c1c30da25a9d
Frankie Ottomanelli <francis.ottomanelli@gmail.com>
2022-08-03 12:22:24 -0400

    use await instead of then for aws

--- src/aws.ts
+++ src/aws.ts
@@ -3,22 +3,17 @@ import AWS from 'aws-sdk'
 AWS.config.update({ region: process.env.AWS_REGION });
 
 export const sendSMS = async (message: string) => {
-    console.log(`Sending SMS message with message: ${message}`);
+  console.log('Sending SMS message')
+  const snsParams = {
+      Message: message,
+      TopicArn: process.env.AWS_TOPIC_ARN,
+  };
 
-    var snsParams = {
-        Message: message,
-        TopicArn: process.env.AWS_TOPIC_ARN,
-    };
-
-    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(snsParams).promise();
-
-    publishTextPromise.then(function(data) {
-        console.log(`Message "${snsParams.Message}" sent to the topic "${snsParams.TopicArn}"`);
-        console.log("MessageID is " + data.MessageId);
-    }).catch(
-        function(err) {
-            console.error(err, err.stack);
-    });
-
-    console.log('Message sent!')
+  try {
+      const publishTextPromise = await new AWS.SNS({apiVersion: '2010-03-31'}).publish(snsParams).promise();
+      console.log(`Message "${snsParams.Message}" sent to the topic "${snsParams.TopicArn}"`);
+      console.log("MessageID is " + publishTextPromise.MessageId); 
+  } catch (e) {
+      console.error(e);
+  }
 }
